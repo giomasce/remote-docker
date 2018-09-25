296e2a1af5b0dc5f62d0678a0674c38e7c4bccff
Giovanni Mascellani <gio@debian.org>
2018-09-25 20:54:37 +0200

    Fix again.

--- client/run.sh
+++ client/run.sh
@@ -5,5 +5,4 @@ echo $SSH_KEY | base64 -d > /root/.ssh/id_rsa
 echo $SSH_KNOWN_HOSTS | base64 -d > /root/.ssh/known_hosts
 chmod 600 /root/.ssh/*
 ssh $SSH_ADDR request-work > /root/work.sh
-chmod 755 /root/work.sh
-/root/work.sh
+bash /root/work.sh
